1cfae530bea9b950b3c0fd5ccca08068831d78ee
Julien Sulpis <julien.sulpis@gmail.com>
2018-05-14 19:28:30 +0200

    fix: update relative paths

--- js/credits.js
+++ js/credits.js
@@ -1,9 +1,9 @@
 $("#yes-btn").on("click", function() {
-    $(".meme").attr("src", "../img/gif/happy.webp");
+    $(".meme").attr("src", "img/gif/happy.webp");
     $(".message").text("Cool! I hope it can help you!");
 })
 
 $("#no-btn").on("click", function() {
-    $(".meme").attr("src", "../img/gif/sad.webp");
+    $(".meme").attr("src", "img/gif/sad.webp");
     $(".message").text("Well... nobody is perfect!");
 })
