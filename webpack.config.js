63fb6e70d8f454c84209788327d70c519482c71d
Julien Sulpis <julien.sulpis@gmail.com>
2018-05-17 00:08:25 +0200

    feat: add npm webpack setup with sass and img loaders

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+
+module.exports = {
+    entry: [
+        './css/index.scss',
+        './css/credits.scss'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            context: 'css/',
+                            outputPath: 'css/',
+                            publicPath: '../'
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: '../img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
