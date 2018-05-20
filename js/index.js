5c900b8a5a7d134d11e8086776f9cfc7a12fcde2
Julien Sulpis <julien.sulpis@gmail.com>
2018-05-20 18:05:21 +0200

    fix: change bootstrap js imports from dist to src

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ import "../css/index.scss";
 
 // Javascript
 import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/bootstrap/js/dist/util.js";
-import "../node_modules/bootstrap/js/dist/collapse.js";
+import "../node_modules/bootstrap/js/src/util.js";
+import "../node_modules/bootstrap/js/src/collapse.js";
 
 // Font Awesome
 import fontawesome from "@fortawesome/fontawesome";
